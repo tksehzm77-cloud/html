4b63e23eb4e0b219ba0fd9cb523f78a7ef17cdc4
tksehzm77-cloud <tksehzm77@gmail.com>
2025-12-24 15:34:26 +0900

    Add files via upload

---
+++ 01_jquery binding/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+// 코드 작성
+// 버튼 click 이벤트 하면
+// div 박스의 내부 색을 red로
+// 변경하는 프로그램
+
+// 버튼을 클릭하면
+$('button').click(function(){
+    // div 박스의 내부색을 red
+    $("div").css({"background":"red"})
+})
+
+
+
+})
