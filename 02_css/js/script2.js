ee6014dd06b75e3fdfed29b7e7daad376d197564
tksehzm77-cloud <tksehzm77@gmail.com>
2025-12-24 16:50:38 +0900

    Add files via upload

---
+++ 02_css/js/script2.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("ul").css({"display":"flex","gap":"20px","margin-left":"50%","transform":"translateX(-50%)"})
+    $("ul li").css({"list-style":"none","width":200,"height":200,"border":"1px solid #000"})
+    $("ul .box1").css({"background":"#fe0000"})
+    $("ul .box2").css({"background":"#fea500"})
+    $("ul .box3").css({"background":"#ffff00"})
+    $("ul .box4").css({"background":"#008001"})
+
+
+
+
+})
