099933b9fd579b47c64cff4351438e937f25cb15
tksehzm77-cloud <tksehzm77@gmail.com>
2025-12-24 16:24:18 +0900

    Add files via upload

---
+++ 02_css/js/script.js
@@ -0,0 +1,14 @@
+// 준비작업
+$(function(){
+    // $("선택자").css({"속성":"값"})
+    $("h1").css({"color":"red"})
+    $(".date").css({"color":"#fff","background":"purple","font-size":80})
+    $(".para").css({"border":"10px solid blue"})
+    $("ul li").css({"border":"1px solid red"})
+    $("ul li:last-child").css({"font-size":50})
+    $("a").css({"border":"5px solid blue","padding":"15px 10px"})
+
+
+
+
+})
