4f110e92d30ff1c0a31a0cadb24bff9b1aa46c9e
tksehzm77-cloud <tksehzm77@gmail.com>
2025-12-26 17:10:42 +0900

    Add files via upload

---
+++ 03_IBK(familysite)/js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    // $("선택자").실행할 함수 이름
+    $(".family2 .family_list").hide()
+    $(".family .family_list").hide()
+
+    // 첫번째 패밀리 버튼을 클릭하면 family list 보임/숨김
+    $(".family button").click(function(){
+        $(".family .family_list").toggle()
+    })
+    $(".family2 button").click(function(){
+        $(".family2 .family_list").toggle()
+    })
+})
