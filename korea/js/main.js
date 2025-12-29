6dcea51bed6c2eb6a40fcd43ad9dab222df5ee26
tksehzm77-cloud <tksehzm77@gmail.com>
2025-12-29 15:55:11 +0900

    Add files via upload

---
+++ korea/js/main.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+    direction: 'horizontal',
+    effect: 'fade',
+    loop: true,
+    autoplay: {
+    delay: 3000,
+    disableOnInteraction: false, // swipe 후에도 계속 자동 실행
+    pauseOnMouseEnter: true,     // 마우스 올리면 정지
+    },
+    
+
+  // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+
+  // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+  // And if we need scrollbar
+    scrollbar: {
+    el: '.swiper-scrollbar',
+    },
+});
+const toggleBtn = document.querySelector('.swiper-btn-toggle');
+
+toggleBtn.addEventListener('click', () => {
+  if (toggleBtn.classList.contains('play')) {
+    swiper.autoplay.start();
+    toggleBtn.classList.remove('play');
+  } else {
+    swiper.autoplay.stop();
+    toggleBtn.classList.add('play');
+  }
+});
