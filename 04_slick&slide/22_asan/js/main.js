98b8203d9cc9444474894b70db6afdbcda62e9a3
tksehzm77-cloud <tksehzm77@gmail.com>
2025-12-29 12:43:26 +0900

    Add files via upload

---
+++ 04_slick&slide/22_asan/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $('.slickslide').slick({
+        // 슬라이드 자동 실행
+    autoplay: true,
+    // 페이지 버튼 보이기
+    dots: true,
+    // 자동 실행시간 조절
+    autoplaySpeed: 2000,
+    // 좌우버튼
+    arrows: false,
+    // fade
+    // fade: true,
+    // 슬라이드 효과를 상하
+    // vertical: true,
+    // 반복하지 않고 한 번만 실행
+    // infinite: false,
+    pauseOnHover: true,
+    slidesToScroll: 1
+    });
+
+
+
+})
