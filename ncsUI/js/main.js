452b35b38ad806f99facafd1a07e9b8a5909bcf1
tksehzm77-cloud <tksehzm77@gmail.com>
2025-12-30 10:50:26 +0900

    Add files via upload

--- ncsUI/js/main.js
+++ ncsUI/js/main.js
@@ -35,6 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-setTimeout(() => {
-  window.location.href = "index2.html";
-}, 10000);
+// setTimeout(() => {
+//   window.location.href = "index2.html";
+// }, 10000);
